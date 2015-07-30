1839c9592075073df6707bf77bc2b3c2c8bf35c8
Amartya Datta Gupta <amartya03@gmail.com>
2015-07-29 23:32:45 -0700

    [Poject] Added a .gitignore for the build dir

--- src/pipeline.c
+++ src/pipeline.c
@@ -6,7 +6,9 @@
 #ifndef TYPE
     #define TYPE   float
 #endif
-#define OFFSET 0
+#ifndef OFFSET
+    #define OFFSET 3
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,14 +16,14 @@
 #include <stdint.h>
 
 // A = c*A
-double scalarMult(TYPE *A, int c, uintptr_t len, int iterations) 
+double scalarMult(TYPE *A, int c, uintptr_t len, long int iterations) 
 {
     struct timespec start, end;
 
     clock_gettime(CLOCK_REALTIME, &start);
     for(int iter=0; iter<iterations; iter++) {
-        for(uintptr_t i=0; i<len; i++) {
-            A[i] = c * A[i];
+        for(uintptr_t i=OFFSET; i<len+OFFSET; i++) {
+            A[i] = c * A[i-OFFSET];
         }
     }
     clock_gettime(CLOCK_REALTIME, &end);
@@ -38,9 +40,17 @@ TYPE * memAllocator(uintptr_t len)
 //Main
 int main(int argc, char *argv[]) 
 {
-    uintptr_t len  = (uintptr_t)atol(argv[1]);
-    int       runs = atoi(argv[2]); 
-    TYPE *A = memAllocator(len);
-    int c=rand()%10;
-    printf("Time taken: %lf nanoseconds\n", scalarMult(A,c,len,runs));
+    uintptr_t maxlen   = (uintptr_t)atol(argv[1]);
+    long int  stepSize = atoi(argv[2]);
+    long int  runs;
+    double    totalTime=0.0d;
+
+    for(uintptr_t len=10; len<maxlen; len+=stepSize) {
+        runs=100*900000/len;
+        TYPE *A = memAllocator(len+OFFSET);
+        int c   = rand()%10;
+        totalTime = scalarMult(A,c,len,runs);
+        printf("%ld , %lf , %lf\n", (long int)len, totalTime, totalTime/len);
+        free(A);
+    }
 }
